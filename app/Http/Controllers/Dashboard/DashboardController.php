361b432c23b4a123314c81a8e044e1c96290a343
Javaughn Bailey <javaughnbailey21@gmail.com>
2021-10-09 12:23:43 -0500

    Controllers Folder Updated
    --Dashboard Controller Updated
    --Homepage Controller Updated
    --Login Controller Updated
    --Register Controller Updated
    --OAuth Controller Added

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Classroom;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+
+    public function index()
+    {
+//        dd(\Auth::id());
+        $userClassrooms = User::with('classroom')->where('user_id',auth()->id())->get();
+//        $userClassrooms = User::find(auth()->id())->with('classroom')->get();
+
+//        dd($userClassrooms);
+
+        return view('user.Dashboard.index',compact('userClassrooms'));
+    }
 }
--- app/Http/Controllers/Log_Reg/LoginController.php
+++ app/Http/Controllers/Log_Reg/LoginController.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Controllers\Log_Reg;
 
+use App\Action\LoginAction;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthenticateUserRequest;
+use App\Models\User;
+use Auth;
+use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LoginController extends Controller
 {
-    //
+    public function index(){
+        return view('Login_Registration.login');
+    }
+
+    public function login(AuthenticateUserRequest $loginRequest, LoginAction $loginAction) {
+
+        $loginAction->execute($loginRequest);
+
+        return redirect('/');
+
+    }
+
+
+    public function logout(){
+        Auth::logout();
+        return redirect('/login');
+    }
+
+
 }
