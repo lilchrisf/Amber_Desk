2f79bb4e428b9b493cb065f2decf9f5376ddd1b9
Javaughn Bailey <javaughnbailey21@gmail.com>
2021-10-09 13:21:22 -0500

    Logo Components Folder Created --Logo Normal Added

--- resources/views/components/User/logo/logo_normal.blade.php
+++ resources/views/components/User/logo/logo_normal.blade.php
@@ -1,5 +1,5 @@
-<div
+<a href="{{route('Homepage')}}"
     class="text-xl font-semibold tracking-widest text-gray-900 uppercase
      rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">
     <span class="text-orange-500">Amber</span>-<span class="text-blue-600">Desk</span>
-</div>
+</a>
