2e7633838473d9e09e4d588d24e3a538d1dfe2e9
Javaughn Bailey <javaughnbailey21@gmail.com>
2021-10-09 17:08:41 -0500

    Classroom Model Modified --Updated Fillable --Added Password Mutator

--- app/Action/StoreClassroomAction.php
+++ app/Action/StoreClassroomAction.php
@@ -16,7 +16,7 @@ class StoreClassroomAction
     public function execute(StoreClassroomRequest $request):void {
 
         Classroom::create([
-            'user_id' => Auth::id(),
+            'teacher_id' => Auth::id(),
             'class_nm' => $request->class_nm,
             'password' => $request->password,
             'invi_link' => trim($request->class_nm.uniqid('', true))
--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -2,29 +2,34 @@
 
 namespace App\Models;
 
+use Hash;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Classroom extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
 
     protected $fillable = [
-        'class_password',
+        'password',
         'teacher_id',
-        'class_name',
+        'class_nm',
         'invi_link',
     ];
 
     protected $primaryKey = 'class_id';
+    protected $table = 'classrooms';
 
-    protected $table = "classrooms";
 
-    public function user(){
-        return $this->belongsTo(User::class,'teacher_id','user_id');
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'teacher_id', 'user_id');
     }
 
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
 }
